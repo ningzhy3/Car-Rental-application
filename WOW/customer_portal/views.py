8c9f2e7846ad017b598578ddd39b38a66ba8f38c
ningzhy3 <zn414@nyu.edu>
2020-12-10 15:38:36 -0500

    Update views.py

--- WOW/customer_portal/views.py
+++ WOW/customer_portal/views.py
@@ -136,10 +136,6 @@ def invoice(request):
     rental_service = Rental_service.objects.get(customer_id = customer) 
 
     invoice = Invoice.objects.get(rental_service = rental_service)
-
-
-    
-
     return render(request, 'customer/invoice.html',{'invoice':invoice})
     
 
