c628eed7134252b5ef25ac006cb0103fb2b31ccd
ningzhy3 <zn414@nyu.edu>
2020-12-11 20:12:37 -0500

    Update admin.py

--- WOW/customer_portal/admin.py
+++ WOW/customer_portal/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Customer)
+admin.site.register(Vehicle)
+
+admin.site.register(Individual)
+
+admin.site.register(Corporate)
+admin.site.register(Corporation)
+
+admin.site.register(Vehicle_class)
+admin.site.register(Coupon)
+admin.site.register(Invoice)
+admin.site.register(Payment)
+admin.site.register(Rental_service)
+admin.site.register(Location)
+
 
 # Register your models here.
+
+
