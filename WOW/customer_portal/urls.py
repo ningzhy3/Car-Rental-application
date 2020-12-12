7a4ab88bac553620500ce84d2328a6a2018d136a
ningzhy3 <zn414@nyu.edu>
2020-12-11 20:12:45 -0500

    Update urls.py

--- WOW/customer_portal/urls.py
+++ WOW/customer_portal/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path,include
 from customer_portal.views import *
 from django.conf.urls import url
+from django.contrib import admin
+admin.site.site_header = 'WOW Admin'
+admin.site.site_title = 'dd'
+admin.site.index_title = 'Welcome to WOW Admin Portal'
+
+
+
 urlpatterns = [
     url(r'^index/$',index),
     url(r'^login/$',login),
@@ -20,6 +27,7 @@ urlpatterns = [
     url(r'^pay_confirmed/',pay_confirmed),
     url(r'^profile/',profile),
     url(r'^invoice_failed/',invoice_failed),
+    url('admin/', lambda x: HttpResponseRedirect('http://localhost:8000/admin/')),
     
     # url(r'^manage/',manage),
     # url(r'^update/',update_order),
